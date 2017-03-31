9d3d51e126a8887360869861edbe9d476f119387
kboygit <heybisayangdako@gmail.com>
2017-03-31 23:25:43 +0000

    built sections for students and teachers, set up routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     devise_for :users
     resources :courses, only: [:index, :show]
     namespace :instructor do
-        resources :courses, only: [:new, :create, :show]
+        resources :courses, only: [:new, :create, :show] do
+            resources :sections, only: [:new, :create]
+        end
     end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
