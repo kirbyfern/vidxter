bd23e49f9bebf10a5e94dd0e62958baea1576ee8
kboygit <heybisayangdako@gmail.com>
2017-03-31 05:16:04 +0000

    created model course,namespace controller,course creation,database

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
     root 'static_pages#index'
     devise_for :users
+    namespace :instructor do
+        resources :courses, only: [:new, :create, :show]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
