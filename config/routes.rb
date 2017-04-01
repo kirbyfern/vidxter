6e3f7125924c67d4f410a59c983b522156ef2586
kboygit <heybisayangdako@gmail.com>
2017-04-01 05:38:50 +0000

    generated model lesson,controller lessons, db table, hooking up actions lessons

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     devise_for :users
     resources :courses, only: [:index, :show]
     namespace :instructor do
+        resources :sections, only: [] do
+            resources :lessons, only: [:new, :create]
+        end
         resources :courses, only: [:new, :create, :show] do
             resources :sections, only: [:new, :create]
         end
