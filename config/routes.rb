dd3ada67f9330610b68d4f8ee56071ad3db02121
kboygit <heybisayangdako@gmail.com>
2017-04-11 16:15:27 -0700

    moved the new lesson form to a modal, removed unnecessary lines of code, manipulated the DOM

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ Rails.application.routes.draw do
     resources :lessons, only: [:show]
     namespace :instructor do
         resources :lessons, only: [:update]
-        resources :sections, only: [:update]
-        resources :sections, only: [] do
-            resources :lessons, only: [:new, :create]
+        resources :sections, only: [:update] do
+            resources :lessons, only: [:create]
         end
         resources :courses, only: [:new, :create, :show] do
             resources :sections, only: [:create]
